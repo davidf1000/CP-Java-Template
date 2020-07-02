1bb20fc48ac6ef5359c64cb27d83628f29f11ad9
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-07-02 21:53:49 +0700

    first commit

---
+++ Template/FastRead.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.time.*;
+import java.io.*;
+public class FastRead {
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+
+    // Driver method to test above method 
+    public static void main(String[] args) 
+    { 
+        FastReader t=new FastReader();
+    }
+} 
